41be308918d81377d9e6a79ddb7baf45136e921c
Steve Unixson <unixson@gmail.com>
2018-11-26 09:21:35 +0300

    removed auth from sign up

--- routes/admin.js
+++ routes/admin.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const auth = require('../controllers/auth');
 const controller = require('../controllers/admin');
 /* GET home page. */
-router.post('/api/v3/signup', auth.TokenMiddleware, auth.setupPost);
+router.post('/api/v3/signup', auth.setupPost);
 router.put('/admin/distribution', auth.TokenMiddleware, controller.updateUserBase);
 router.get('/admin/statistics', auth.TokenMiddleware, controller.renderStats);
 router.get('/admin/remove', auth.TokenMiddleware, controller.renderRemove);
--- routes/login.js
+++ routes/login.js
@@ -5,7 +5,7 @@ const config = require('../config/config');
 const auth = require('../controllers/auth');
 /* GET home page. */
 router.post('/api/v3/login', auth.login);
-router.post(`${config.url}/remove`, auth.TokenMiddleware, auth.removeUser);
+router.post(`${config.url}/remove`, auth.removeUser);
 router.get('/login', auth.renderLogin);
 router.get('/logout', auth.logout);
 
